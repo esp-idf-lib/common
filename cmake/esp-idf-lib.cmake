12347bc4f4f897838b9bab78a6c24e5fe46aae02
Tomoyuki Sakurai <y@trombik.org>
2025-07-30 12:53:55 +0700

    ci: enable flags that may cause failure only in CI

--- cmake/esp-idf-lib.cmake
+++ cmake/esp-idf-lib.cmake
@@ -1,9 +1,30 @@
-# this file is included after idf_component_register() in
-# CMakeLists.txt.
+# Set common build flags but enable them only when the build is in our CI,
+# making them optional. The idea is, fail when compiled in our CI but just
+# warn in any other cases. Code that compiles with one toolchain warning-free
+# may not do so with another toolchain, which creates a project dependency on
+# specific toolchain vendors and versions.
+#
+# Define flags that may cause failures here.
+if (DEFINED ENV{ESP_IDF_LIB_CI})
+    set(ESP_IDF_LIB_CI_FLAGS
+        -Werror=unused-variable
+        -Werror=unused-function
+        -Werror=write-strings
+        -Werror
+    )
+endif()
 
-# set common build flags
-target_compile_options(${COMPONENT_LIB} PRIVATE
+# Set common build flags. Mandatory.
+#
+# Define flags that do not cause failures here.
+set(ESP_IDF_LIB_FLAGS
+    -Wextra
     -Wwrite-strings
     -Wunused-variable
-    -Werror
+    -Wunused-function
+    -Wreturn-type
+)
+target_compile_options(${COMPONENT_LIB} PRIVATE
+    ${ESP_IDF_LIB_FLAGS}
+    ${ESP_IDF_LIB_CI_FLAGS}
 )
